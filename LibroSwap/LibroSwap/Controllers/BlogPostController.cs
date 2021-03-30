b86d28b7ceb6bc7841f0a1c9a301a8b85bd83074
irabihanska <irabihanska0@gmail.com>
2021-03-30 11:10:24 +0300

    Add new project

---
+++ LibroSwap/LibroSwap/Controllers/BlogPostController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibroSwap.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
