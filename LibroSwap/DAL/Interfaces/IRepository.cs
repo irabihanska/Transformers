301a5825f4e38f817d288bd20eda9e25fd5d077c
ckorpenka <oksana.pulchritudo@gmail.com>
2021-04-08 13:25:32 +0300

    Added Repository class. Installed Automapper

---
+++ LibroSwap/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<T> where T : Entity
+    {
+        //Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        //Task Create(T item);
+        //Task Update(T item);
+        //Task Delete(int id);
+    }
+}
