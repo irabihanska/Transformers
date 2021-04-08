100bccb9d44863277ecbaedf4b23b35f3068ea8e
Julia <julia.legedza@gmail.com>
2021-04-08 15:18:37 +0300

    updated files from projecte

---
+++ Libro_Swap/Libro_Swap/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Libro_Swap.Areas.Identity.Data;
+using Libro_Swap.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Libro_Swap.Areas.Identity.IdentityHostingStartup))]
+namespace Libro_Swap.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Libro_SwapDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Libro_SwapDBContextConnection")));
+
+                services.AddDefaultIdentity<Libro_SwapUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<Libro_SwapDBContext>();
+            });
+        }
+    }
+}
