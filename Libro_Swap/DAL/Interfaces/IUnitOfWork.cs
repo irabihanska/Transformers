dae2dde652cfacd9d96dc22dc2148285fc5cf3ab
ckorpenka <oksana.pulchritudo@gmail.com>
2021-04-12 22:51:56 +0300

    Added files for UoF structure

---
+++ Libro_Swap/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IBookCoverageRepository CoverageRepository { get; }
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
---
+++ Libro_Swap/DAL/Repositories/BookCoveragesRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DAL.Interfaces;
+using DAL.Models;
+
+namespace DAL.Repositories
+{
+    class BookCoveragesRepository : Repository<BookCoverage>, IBookCoverageRepository
+    {
+        public BookCoveragesRepository(Libro_SwapDBContext context, IMapper mapper) : base(context, mapper)
+        {
+
+        }
+    }
+}
