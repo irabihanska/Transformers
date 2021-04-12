8b0142550a28eae420eb1fe6711dd75a8a4568bf
ckorpenka <oksana.pulchritudo@gmail.com>
2021-04-12 22:37:21 +0300

    Added project for new db

---
+++ Libro_Swap/DAL/Libro_SwapDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DAL.Models;
+
+namespace DAL
+{
+    public class Libro_SwapDBContext : IdentityDbContext<Libro_SwapUser>
+    {
+        public Libro_SwapDBContext(DbContextOptions<Libro_SwapDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Libro_Swap/DAL/Models/Libro_SwapUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Models
+{
+    // Add profile data for application users by adding properties to the Libro_SwapUser class
+    public class Libro_SwapUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
