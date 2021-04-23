a7e39250f5a04d9c35235663a2ac1082bed50715
irabihanska <irabihanska0@gmail.com>
2021-04-23 18:41:58 +0300

    Add list of books

---
+++ Libro_Swap/BusinessLogic/Services/BookService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BusinessLogic.Interfaces;
+using DAL.Interfaces;
+using DAL.Models;
+using AutoMapper;
+using Common.DTO;
+
+namespace BusinessLogic.Services
+{
+    public class BookService : IBookService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        private IMapper _mapper;
+
+        public BookService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+        }
+
+        public async Task<BookDTO> Get(int id)
+        {
+            var item = await _unitOfWork.BookRepository.Get(id);
+
+            return _mapper.Map<Book, BookDTO>(item);
+        }
+
+        public async Task<List<BookDTO>> GetAll()
+        {
+            var items = await _unitOfWork.BookRepository.GetAll();
+
+            return _mapper.Map<List<Book>, List<BookDTO>>(items);
+        }
+
+        public async Task<BookDTO> Create(BookDTO item)
+        {
+            var newItem = _mapper.Map<BookDTO, Book>(item);
+
+            await _unitOfWork.BookRepository.Create(newItem);
+            await _unitOfWork.SaveAsync();
+            
+            return _mapper.Map<Book, BookDTO>(newItem);
+        }
+
+        public async Task<BookDTO> Update(BookDTO item)
+        {
+            var updItem = _mapper.Map<BookDTO, Book>(item);
+
+            await _unitOfWork.BookRepository.Update(updItem);
+            await _unitOfWork.SaveAsync();
+
+            return _mapper.Map<Book, BookDTO>(updItem);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _unitOfWork.BookRepository.Delete(id);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
---
+++ Libro_Swap/Libro_Swap/Migrations/20210413191243_allow_empty_about_me.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Libro_Swap.Migrations
+{
+    public partial class allow_empty_about_me : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "about_me",
+                table: "Users",
+                type: "nvarchar(max)",
+                maxLength: 2147483647,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 2147483647);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "about_me",
+                table: "Users",
+                type: "nvarchar(max)",
+                maxLength: 2147483647,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 2147483647,
+                oldNullable: true);
+        }
+    }
+}
